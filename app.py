aafd48f43272a8ba8285b4061f2e5948cfe50f77
TTYBEJT <pasha.kefer@gmail.com>
2023-04-04 07:39:22 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import telebot
+
+
+TOKEN = "6016539624:AAFVQDjxs1Ig1uOgQT7Skrb4VT14rHFgkgc"
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+def username_(message):
+    username = message.from_user.username
+    username = message.from_user.first_name if not username else username
+    return username
+
+
+
+
